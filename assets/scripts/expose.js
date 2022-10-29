c1cea35ffcceb59ca0cc8814d610732cc837188e
bsnow1400 <63069372+bsnow1400@users.noreply.github.com>
2022-10-29 15:23:24 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,47 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  
+  //variables
+  const horn = document.getElementById('horn-select');
+  const volume = document.getElementById('volume');
+  const button = document.getElementsByTagName('button')[0];
+  
+  const confetti = new JSConfetti();
+  
+  const volMute = 'assets/icons/volume-level-0.svg';
+  const volLow = 'assets/icons/volume-level-1.svg'; 
+  const volMed = 'assets/icons/volume-level-2.svg';
+  const volHigh = 'assets/icons/volume-level-3.svg';
+  
+  horn.addEventListener('change', (event) => {
+    const select = event.target.value;
+    const img = document.getElementsByTagName('img')[0];
+    img.src = `assets/images/${select}.svg`;
+    const audio = document.getElementsByClassName('hidden')[0];
+    audio.src = `assets/audio/${select}.mp3`;
+  })
+  
+  volume.addEventListener('change', (event) => {
+    const img = document.getElementsByTagName('img')[1];
+    const audio = document.getElementsByClassName('hidden');
+    const vol = event.target.value;
+    if(vol == 0){
+      img.source = volMute;
+    } else if(vol > 0 && vol < 33){
+      img.source = volLow;
+    } else if(vol >= 33 && vol < 67){
+      img.source = volMed;
+    } else{
+      img.source = volHigh;
+    }
+  })
+  
+  button.addEventListener('change', (event) => {
+    const audio = document.getElementsByClassName('hidden')[0];
+    audio.play();
+    if(horn.value == 'party-horn'){
+      jsConfetti.addConfetti();
+    }
+  }
 }
