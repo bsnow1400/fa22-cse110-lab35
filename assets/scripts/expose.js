acd982e0776124d65cbcc737f0f2c0307ab2b3f9
bsnow1400 <63069372+bsnow1400@users.noreply.github.com>
2022-10-29 16:56:34 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,10 +4,12 @@ window.addEventListener('DOMContentLoaded', init);
   
 const confetti = new JSConfetti();
 
+/*
 const volMute = 'assets/icons/volume-level-0.svg';
 const volLow = 'assets/icons/volume-level-1.svg'; 
 const volMed = 'assets/icons/volume-level-2.svg';
 const volHigh = 'assets/icons/volume-level-3.svg';
+*/
 
 function init() {
   
@@ -26,21 +28,21 @@ function init() {
     const vol = event.target.value;
     const image = document.getElementsByTagName('img')[1];
     if(vol == 0){
-      image.src = 'assets/icons/volume-level-0.svg'
+      image.src = 'assets/icons/volume-level-0.svg';
     } else if(vol > 0 && vol < 33){
-      image.src = 'assets/icons/volume-level-1.svg'
+      image.src = 'assets/icons/volume-level-1.svg';
     } else if(vol >= 33 && vol < 67){
-      image.src = 'assets/icons/volume-level-2.svg'
+      image.src = 'assets/icons/volume-level-2.svg';
     } else{
-      image.src = 'assets/icons/volume-level-3.svg'
+      image.src = 'assets/icons/volume-level-3.svg';
     }
     const audio = document.getElementsByClassName('hidden')[0];
     audio.volume = vol/100;
   })
   
-  button.addEventListener('change', (event) => {
-    const audio = document.getElementsByClassName('hidden')[0];
-    audio.play();
+  button.addEventListener('click', (event) => {
+    const getAudio = document.getElementsByClassName('hidden')[0];
+    getAudio.play();
     if(horn.value == 'party-horn'){
       jsConfetti.addConfetti();
     }
