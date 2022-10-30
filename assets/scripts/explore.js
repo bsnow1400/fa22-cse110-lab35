729ee9521794f09b2c44189ac8a949fd920e826a
wozzack <k1truong@ucsd.edu>
2022-10-29 23:16:37 -0700

    omg

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -10,21 +10,8 @@ function init() {
   {
     let voiceSelect = document.getElementById("voice-select");
     voices = window.speechSynthesis.getVoices();
-    
-    for (let i = 0; i < voices.length ; i++) 
-    {
-      const option = document.createElement('option');
-      option.textContent = `${voices[i].name} (${voices[i].lang})`;
-  
-      if (voices[i].default) 
-      {
-        option.textContent += ' — DEFAULT';
-      }
-  
-      option.setAttribute('data-lang', voices[i].lang);
-      option.setAttribute('data-name', voices[i].name);
-      voiceSelect.appendChild(option);
-    }
+
+  voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
   }
   /*
   const voiceSelect = document.getElementById("voice-select");
