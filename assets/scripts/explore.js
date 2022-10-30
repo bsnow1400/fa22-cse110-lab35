b397338e9715777fddf62b11abf5916124322a58
wozzack <k1truong@ucsd.edu>
2022-10-29 17:49:57 -0700

    added voice listings to explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  const voices = document.querySelector("select");
+
+  function buildVoices()
+  {
+    let voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++)
+    {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default)
+      {
+        option.textContent += ' -- DEFAULT';
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voices.appendChild(option);
+    }
+  }
+  
+  buildVoices();
+
 }
