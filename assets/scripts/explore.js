ea3a518229fcfa7f70109e9bd8ab624d32825126
wozzack <k1truong@ucsd.edu>
2022-10-29 18:10:32 -0700

    gdfsg

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -9,26 +9,21 @@ function init() {
 
   let voices = [];
 
-  function buildVoices()
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++)
   {
-    voices = synth.getVoices();
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
-    for (let i = 0; i < voices.length; i++)
+    if (voices[i].default)
     {
-      const option = document.createElement("option");
-      option.textContent = `${voices[i].name} (${voices[i].lang})`;
-
-      if (voices[i].default)
-      {
-        option.textContent += ' -- DEFAULT';
-      }
-
-      option.setAttribute("data-lang", voices[i].lang);
-      option.setAttribute("data-name", voices[i].name);
-      voiceSelect.appendChild(option);
+      option.textContent += ' -- DEFAULT';
     }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
   }
-
-  buildVoices();
-
 }
+
