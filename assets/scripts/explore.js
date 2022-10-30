919945ceecb93d6fa5fd5e3789426e1ade7cf902
wozzack <k1truong@ucsd.edu>
2022-10-29 22:49:52 -0700

    sv

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,25 +5,34 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
 
   const synth = window.speechSynthesis;
-  const voiceSelect = document.querySelector("select");
+  const voiceSelect = document.querySelector('select');
 
-  let voices = [];
-
-  voices = synth.getVoices();
-
-  for (let i = 0; i < voices.length; i++)
+  function populateVoiceList() 
   {
-    const option = document.createElement("option");
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
-
-    if (voices[i].default)
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length ; i++) 
     {
-      option.textContent += ' -- DEFAULT';
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) 
+      {
+        option.textContent += ' — DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
     }
-
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    voiceSelect.appendChild(option);
   }
+  
+  populateVoiceList();
+  
+  if (speechSynthesis.onvoiceschanged !== undefined) 
+  {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  
 }
 
